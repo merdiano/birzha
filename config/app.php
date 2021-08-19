ce4893d65e45510eb4f9eafb36ec8945766a991b
saparatayev <sopa.97@mail.ru>
2021-08-19 15:19:09 +0500

    added env file

--- config/app.php
+++ config/app.php
@@ -16,7 +16,7 @@ return [
     |
     */
 
-    'debug' => true,
+    'debug' => env('APP_DEBUG', true),
 
     /*
     |--------------------------------------------------------------------------
@@ -41,7 +41,7 @@ return [
     |
     */
 
-    'url' => 'http://localhost',
+    'url' => env('APP_URL', 'http://localhost'),
 
     /*
     |--------------------------------------------------------------------------
@@ -137,7 +137,7 @@ return [
     |
     */
 
-    'key' => 'S6g15W4dDKtMwoxFbvFRPEtFlEMDT661',
+    'key' => env('APP_KEY', ''),
 
     'cipher' => 'AES-256-CBC',
 
