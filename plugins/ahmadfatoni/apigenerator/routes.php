1fc5bf9912f862d7bd02935afa35c8166052e32e
saparatayev <sopa.97@mail.ru>
2021-08-28 15:47:14 +0500

    fix: categories api routes

--- plugins/ahmadfatoni/apigenerator/routes.php
+++ plugins/ahmadfatoni/apigenerator/routes.php
@@ -4,7 +4,7 @@ Route::post('fatoni/generate/api', array('as' => 'fatoni.generate.api', 'uses' =
 Route::post('fatoni/update/api/{id}', array('as' => 'fatoni.update.api', 'uses' => 'AhmadFatoni\ApiGenerator\Controllers\ApiGeneratorController@updateApi'));
 Route::get('fatoni/delete/api/{id}', array('as' => 'fatoni.delete.api', 'uses' => 'AhmadFatoni\ApiGenerator\Controllers\ApiGeneratorController@deleteApi'));
 
-Route::resource('api/v1/categories', 'AhmadFatoni\ApiGenerator\Controllers\API\Categories APIController', ['except' => ['destroy', 'create', 'edit']]);
-Route::get('api/v1/categories/{id}/delete', ['as' => 'api/v1/categories.delete', 'uses' => 'AhmadFatoni\ApiGenerator\Controllers\API\Categories APIController@destroy']);
+Route::resource('api/v1/categories', 'AhmadFatoni\ApiGenerator\Controllers\API\CategoriesAPIController', ['except' => ['destroy', 'create', 'edit']]);
+Route::get('api/v1/categories/{id}/delete', ['as' => 'api/v1/categories.delete', 'uses' => 'AhmadFatoni\ApiGenerator\Controllers\API\CategoriesAPIController@destroy']);
 Route::resource('api/v1/products', 'AhmadFatoni\ApiGenerator\Controllers\API\ProductsAPIController', ['except' => ['destroy', 'create', 'edit']]);
 Route::get('api/v1/products/{id}/delete', ['as' => 'api/v1/products.delete', 'uses' => 'AhmadFatoni\ApiGenerator\Controllers\API\ProductsAPIController@destroy']);
