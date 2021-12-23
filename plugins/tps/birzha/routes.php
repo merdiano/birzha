3287a2936044db6bbfe36c94b3f5fe55efdc9961
saparatayev <sopa.97@mail.ru>
2021-12-23 13:08:05 +0500

    test sms server

--- plugins/tps/birzha/routes.php
+++ plugins/tps/birzha/routes.php
@@ -1,5 +1,8 @@
 <?php
 use Illuminate\Support\Facades\Route;
+use October\Rain\Network\Http;
+use October\Rain\Support\Facades\Http as FacadesHttp;
+
 Route::namespace('TPS\Birzha\Controllers')->group(function () {
 
     Route::prefix('api')->group(function (){
@@ -10,3 +13,25 @@ Route::namespace('TPS\Birzha\Controllers')->group(function () {
 
 
 // Route::get('bank_result/{payment_id}', ['as'=>'paymentReturn','uses'=>'...@checkPayment'] );
+
+Route::get('check-sms', function() {
+    // $response = Http::withHeaders([
+    //     'Content-Type' => 'application/json'
+        
+    // ])->post('http://217.174.228.218/auth/jwt/create', [
+    //                                         'username' => 'birja',
+    //                                         'password' => 'Birj@1',
+    //                                     ])->throw()->json();
+
+    // $accessToken = $response['access'];
+
+    // dd($accessToken);
+
+    $client = Http::make('https://217.174.228.218:5019/auth/jwt/create', Http::METHOD_POST)->data([
+        'username' => 'birja',
+        'password' => 'Birj@1',
+    ])->timeout(3600);
+
+    $client->setOption(CURLOPT_POSTFIELDS,$client->getRequestData());
+    dd($client->send());
+});
