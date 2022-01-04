56a5a14318c16816c13c4488d978e1dc80cf4890
saparatayev <sopa.97@mail.ru>
2022-01-04 10:53:26 +0500

    test sms 50

--- plugins/tps/birzha/routes.php
+++ plugins/tps/birzha/routes.php
@@ -20,9 +20,11 @@ Route::namespace('TPS\Birzha\Controllers')->group(function () {
 Route::get('tm/check-sms', function() {
     
     $tx=new SMPP('217.174.228.218', 5019); // make sure the port is integer
-    $tx->debug=false;
+    $tx->debug=true;
     $tx->bindTransmitter("birja","Birj@1");
+    dump('bind transmitter');
     $result = $tx->sendSMS("0773","99365611968","Hello world");
+    dump('send sms attempt');
     echo $tx->getStatusMessage($result);
     $tx->close();
     unset($tx);
