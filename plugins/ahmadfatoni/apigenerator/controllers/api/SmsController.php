e8b5c1501c8cad8860cff0f4bd9fe10668c4bd23
merdan <merdan.m@gmail.com>
2022-01-29 12:41:02 +0500

    sms test

--- plugins/ahmadfatoni/apigenerator/controllers/api/SmsController.php
+++ plugins/ahmadfatoni/apigenerator/controllers/api/SmsController.php
@@ -8,7 +8,7 @@ use LaravelSmpp\SmppServiceInterface;
 class SmsController extends Controller
 {
     public function index(SmppServiceInterface $smpp) {
-        $this->smpp->sendOne(99363432211, 'Hi, this SMS was send via SMPP protocol');
+        $smpp->sendOne(99363432211, 'Hi, this SMS was send via SMPP protocol');
         return 'success';
     }
 }
